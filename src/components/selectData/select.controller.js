fc7fecaa3b6681c8f04b7c64341eb2b8ad776566
bordaquievich@hotmail.com <bordaquievich@hotmail.com>
2017-05-07 11:17:13 -0300

    se agrego la posibilidad de agregar subcampos al titulo del selectdata

--- src/components/selectData/select.controller.js
+++ src/components/selectData/select.controller.js
@@ -1,4 +1,4 @@
-(function(angular) { 
+(function(angular) {
     'use strict';
     angular
         .module('uiComponents.components')
@@ -30,12 +30,27 @@
 
             if (select.selected !== undefined) {
                 for (var index in vm.configuracion.campos) {
-                    text += select.selected[vm.configuracion.campos[index]];
-                    if (index < vm.configuracion.campos.length) { text += ' - '; }
+                    text += byString(select.selected, vm.configuracion.campos[index]);
+                    text += ' - ';
                 }
-
+                text = text.substr(0, text.length - 3);
             }
             return text;
+        }
+
+        function byString(o, s) {
+            s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+            s = s.replace(/^\./, ''); // strip a leading dot
+            var a = s.split('.');
+            for (var i = 0, n = a.length; i < n; ++i) {
+                var k = a[i];
+                if (k in o) {
+                    o = o[k];
+                } else {
+                    return;
+                }
+            }
+            return o;
         }
 
         function onSelect(item) {
