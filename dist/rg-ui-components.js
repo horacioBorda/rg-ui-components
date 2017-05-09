b10a20628dc2d25d42aaddbd21829fc6b32ff1f1
bordaquievich@hotmail.com <bordaquievich@hotmail.com>
2017-05-08 22:26:49 -0300

    deploy

--- dist/rg-ui-components.js
+++ dist/rg-ui-components.js
@@ -1,123 +1,125 @@
-(function(angular) {
 
-    // Create all modules and define dependencies to make sure they exist
-    // and are loaded in the correct order to satisfy dependency injection
-    // before all nested files are concatenated by Gulp
+(function (angular) {
 
-    // Config
+  // Create all modules and define dependencies to make sure they exist
+  // and are loaded in the correct order to satisfy dependency injection
+  // before all nested files are concatenated by Gulp
 
-    angular.module('uiComponents.config', [])
-        .value('uiComponents.config', {
-            debug: true
-        })
-        .config(generalConfig);
+  // Config
 
-    function generalConfig($provide) {
-        // PARA AGREGAR EL LENGUAGE DE TODOS LOS UI-GRID EN ESPAÑOL
-        $provide.decorator('GridOptions', ['$delegate', 'i18nService', function($delegate, i18nService) {
-            var gridOptions;
-            gridOptions = angular.copy($delegate);
-            gridOptions.initialize = function(options) {
-                var initOptions;
-                initOptions = $delegate.initialize(options);
-                return initOptions;
-            };
-            //es is the language prefix you want
-            i18nService.setCurrentLang('es');
-            return gridOptions;
-        }]);
-    }
-    angular.module('uiComponents.filters', []);
-    angular.module('uiComponents.components', [
-        'ui.grid',
-        'ui.grid.exporter',
-        'ui.grid.selection',
-        'ui.grid.pagination',
-        'ui.grid.pinning',
-        'ui.grid.moveColumns',
-        'ui.grid.edit',
-        'ui.grid.rowEdit'
-    ]);
-    angular.module('uiComponents', [
-        'uiComponents.config',
-        'uiComponents.components',
-        'uiComponents.filters'
-    ]);
+  angular.module('uiComponents.config', [])
+      .value('uiComponents.config', {
+          debug: true
+      })
+      .config(generalConfig);
 
-})(angular);
-
-
-(function(angular) {
-    'use strict';
-    angular.module('uiComponents.filters')
-        .filter('propsFilter', propsFilter);
-
-
-    function propsFilter() {
-        return function(items, props) {
-            var out = [];
-
-            if (angular.isArray(items)) {
-                var keys = Object.keys(props);
-
-                items.forEach(function(item) {
-                    var itemMatches = false;
-
-                    for (var i = 0; i < keys.length; i++) {
-                        var prop = keys[i];
-                        var text = props[prop].toLowerCase();
-                        if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
-                            itemMatches = true;
-                            break;
-                        }
-                    }
-
-                    if (itemMatches) {
-                        out.push(item);
-                    }
-                });
-            } else {
-                // Let the output be the input untouched
-                out = items;
-            }
-
-            return out;
+  function generalConfig($provide){
+     // PARA AGREGAR EL LENGUAGE DE TODOS LOS UI-GRID EN ESPAÑOL
+    $provide.decorator('GridOptions',['$delegate', 'i18nService', function($delegate, i18nService){
+        var gridOptions;
+        gridOptions = angular.copy($delegate);
+        gridOptions.initialize = function(options) {
+            var initOptions;
+            initOptions = $delegate.initialize(options);
+            return initOptions;
         };
-    }
-})(angular);
-(function(angular) {
-    'use strict';
-    angular.module('uiComponents.filters', []);
-})(angular);
-(function(angular) {
-    'use strict';
+        //es is the language prefix you want
+        i18nService.setCurrentLang('es');
+        return gridOptions;
+    }]);
+  }    
+  angular.module('uiComponents.filters',[]);
+  angular.module('uiComponents.components',[
+    'ui.grid',
+    'ui.grid.exporter',
+    'ui.grid.selection',
+    'ui.grid.pagination',
+    'ui.grid.pinning',
+    'ui.grid.moveColumns',
+    'ui.grid.edit', 
+    'ui.grid.rowEdit'
+    ]);
+  angular.module('uiComponents',
+      [
+          'uiComponents.config',
+          'uiComponents.components',
+          'uiComponents.filters'
+      ]);
 
-    var listadoEntidad = {
-        restrict: 'E',
-        template: '<div class="myGrid" id="grid1" ui-grid="lrc.optionsEntidades" ' +
-            ' ui-grid-exporter  ui-grid-selection  ui-grid-expandable ui-grid-pagination ui-grid-edit  ' +
-            'ui-grid-row-edit ui-grid-move-columns ui-grid-pinning ui-grid-resize-columns  bs-loading-overlay></div>',
-        controller: 'ListadoEntidadController as lrc',
-        bindings: {
-            selectAll: '=?',
-            deselectedAll: '=?',
-            entidades: '=?',
-            entidadesSeleccionadas: '=?',
-            servicio: '=?', // esto sirve para poder procesar los datos para pasarlo al componente, cada servicio debe implementar el metodo !!! obtenerDatos()  !!!!
-            gridOptions: '<',
-            config: '<?',
-            path: '<?',
-            clickItem: '&?',
-            metodos: '=?' //aca van todos los metodos que quiero controlar dentro del componente, seria un objeto metodo con varias funciones
+})(angular);
+
+
+(function (angular) {
+  'use strict';
+  angular.module('uiComponents.filters')
+  .filter('propsFilter',propsFilter);
+
+
+function propsFilter(){
+  return function(items, props) {
+    var out = [];
+
+    if (angular.isArray(items)) {
+      var keys = Object.keys(props);
+        
+      items.forEach(function(item) {
+        var itemMatches = false;
+
+        for (var i = 0; i < keys.length; i++) {
+          var prop = keys[i];
+          var text = props[prop].toLowerCase();
+          if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
+            itemMatches = true;
+            break;
+          }
         }
+
+        if (itemMatches) {
+          out.push(item);
+        }
+      });
+    } else {
+      // Let the output be the input untouched
+      out = items;
+    }
+
+    return out;
+  };
+}
+})(angular); 
+(function (angular) {
+	'use strict';
+	angular.module('uiComponents.filters',[]);
+})(angular); 
+ (function (angular) {
+     'use strict';
+
+      var listadoEntidad = {
+            restrict:'E',
+            template: '<div class="myGrid" id="grid1" ui-grid="lrc.optionsEntidades" '+
+            ' ui-grid-exporter  ui-grid-selection  ui-grid-expandable ui-grid-pagination ui-grid-edit  '+
+            'ui-grid-row-edit ui-grid-move-columns ui-grid-pinning ui-grid-resize-columns  bs-loading-overlay></div>',
+            controller: 'ListadoEntidadController as lrc',
+            bindings:{
+                selectAll:'=?',
+                deselectedAll:'=?',
+                entidades:'=?',
+                entidadesSeleccionadas:'=?',
+                servicio: '=?', // esto sirve para poder procesar los datos para pasarlo al componente, cada servicio debe implementar el metodo !!! obtenerDatos()  !!!!
+                gridOptions: '<',
+                config:'<?',
+                path: '<?',
+                clickItem: '&?',
+                metodos: '=?' //aca van todos los metodos que quiero controlar dentro del componente, seria un objeto metodo con varias funciones
+            }
     };
-    angular
-        .module('uiComponents.components')
-        .component('listadoEntidad', listadoEntidad);
+      angular
+      .module('uiComponents.components')
+      .component('listadoEntidad',listadoEntidad);
 
-
-})(angular);
-
+     
+ })(angular); 
+ 
 (function(angular) {
     'use strict';
     angular.module('uiComponents.components')
@@ -256,50 +258,51 @@
 // que sea general hay que definir un servicio generico que consulte un url y la parsee a un json.
 (function(angular) {
     'use strict';
-    var selectData = {
+     var selectData = {
         restrict: 'E',
-        template: '<form class="form-horizontal">' +
-            '<small >{{sc.configuracion.nombre}}</small>' +
-            '<div class="form-group">' +
-            '<ui-select class="btn-group bootstrap-select form-control" search-enabled="sc.searchEnabled" ng-model="sc.ultimaEntidad" ' +
-            ' title="{{sc.configuracion.titulo}}" on-select="sc.onSelect($item)" append-to-body="true" >' +
-            '<ui-select-match placeholder="{{sc.configuracion.placeholder}}" popover-popup-delay="750" uib-popover="{{sc.configuracion.toolTip}}" ' +
-            'popover-trigger="mouseenter" popover-placement="bottom">{{sc.cargarCampos($select)}}' +
-            '</ui-select-match><ui-select-choices repeat="entidad in sc.entidades | propsFilter: sc.getSearch($select)">' +
-            '<div><span>{{sc.configuracion.campos[0].title}} </span> <span ng-bind-html="entidad[sc.configuracion.campos[0].field] | highlight: $select.search">' +
-            '</span></div><small ng-repeat="subcampo in sc.configuracion.subcampos">' +
-            '{{subcampo.title}}: <span ng-bind-html="\'\'+ sc.byString(entidad,subcampo.field) | highlight: $select.search">' +
-            '</span><!--  email: {{person.email}}age: <span ng-bind-html="\'\'+person.age | highlight: $select.search">' +
-            '</span> --></small></ui-select-choices> </ui-select></div></form>',
+        template: '<form class="form-horizontal">'+
+        '<small >{{sc.configuracion.nombre}}</small>'+
+        '<div class="form-group">'+
+        '<ui-select class="btn-group bootstrap-select form-control" search-enabled="sc.searchEnabled" ng-model="sc.ultimaEntidad" '+
+        ' title="{{sc.configuracion.titulo}}" on-select="sc.onSelect($item)" append-to-body="true" >'+
+        '<ui-select-match placeholder="{{sc.configuracion.placeholder}}" popover-popup-delay="750" uib-popover="{{sc.configuracion.toolTip}}" '+
+        'popover-trigger="mouseenter" popover-placement="bottom">{{sc.cargarCampos($select)}}'+
+        '</ui-select-match><ui-select-choices repeat="entidad in sc.entidades | propsFilter: sc.getSearch($select)">'+
+        '<div><span>{{sc.configuracion.campos[0].title}} </span> <span ng-bind-html="entidad[sc.configuracion.campos[0].field] | highlight: $select.search">'+
+        '</span></div><small ng-repeat="subcampo in sc.configuracion.subcampos">'+
+        '{{subcampo.title}}: <span ng-bind-html="\'\'+ sc.byString(entidad,subcampo.field) | highlight: $select.search">'+
+        '</span><!--  email: {{person.email}}age: <span ng-bind-html="\'\'+person.age | highlight: $select.search">'+
+        '</span> --></small></ui-select-choices> </ui-select></div></form>',
         controller: 'SelectController as sc',
         bindings: {
-            url: '<?', //esta url no se utiliza por el momkento, pero esta pensado para que sea utilizado por el servicio 
+            url: '<?',//esta url no se utiliza por el momkento, pero esta pensado para que sea utilizado por el servicio 
             // campos: '<?',
             // subcampos:'<?',
             // title: '@',
             // placeholder: '@', ESTOS CAMPOS VAN A SER REEMPLAZADOS POR CONFIGURACION
-            configuracion: '<?',
+            configuracion:'<?',
             //el objeto configuracion debe tener la siguiente forma:
             //configuracion:{
-            //                  titulo: STRING "titulo de la entidad" 
-            //                  campos: ARRAY STRING ,campos que se quiere mostrar, 
-            //                  subcampos: ARRAY STRING "Subcampos que se quiere mostrar"
-            //                  toolTip: STRING para mostrar un tooltip
-            //                  placholder: STRING 
-            //}
-            searchEnabled: '<?', //true|false, default: true 
-            servicio: '<?', // este servicio debe implementar obtenerDatos() y debe devolver un promise con los datos ya resueltos
+//                  titulo: STRING "titulo de la entidad" 
+//                  campos: ARRAY STRING ,campos que se quiere mostrar, 
+//                  subcampos: ARRAY STRING "Subcampos que se quiere mostrar"
+//                  toolTip: STRING para mostrar un tooltip
+//                  placholder: STRING,
+//                  fieldsToSearch: ARRAY, campos por los que se van a buscar, si no se pasa nada por defecto busca por todos los campos. 
+                //}
+            searchEnabled:'<?',   //true|false, default: true 
+            servicio: '<?',  // este servicio debe implementar obtenerDatos() y debe devolver un promise con los datos ya resueltos
             parametros: '<?', // parametros del servicio obtenerDatos, si no se lo pasa va como undefined
-            entidades: '<?', // si se traen entidades, no se consulta a servicio
+            entidades: '<?',    // si se traen entidades, no se consulta a servicio
             ultimaEntidad: '=?', // entidad que se  va a mostrar como seleccionada, default: la primera de la lista
             clickItem: '&?' // evento que se dispara cuando se selecciona un item, $event es el item seleccionado
         }
     };
     angular
         .module('uiComponents.components')
-        .component('selectData', selectData);
-
-
+        .component('selectData',selectData);
+        
+   
 })(angular);
 (function(angular) {
     'use strict';
@@ -322,30 +325,20 @@
         vm.$onInit = function() {
             checkDatos();
         };
-
-        function getSearch($select) {
-            var toSearch = {};
-            if (vm.configuracion.fieldsToSearch === undefined) {//SI NO SE PASO COMO PARAMETRO POR QUE CRITERIOS BUSCAR, BUSCAR POR TODO
-                vm.configuracion.campos.forEach(function(campo) {
-                    toSearch[campo.field] = $select.search;
-
-                });
-                vm.configuracion.subcampos.forEach(function(subcampo) {
-                    toSearch[subcampo.field] = $select.search;
-                });
-            } else { // BUSCAR POR LOS CAMPOS QUE SE PASARON COMO PARAMETRO
-                vm.configuracion.fieldsToSearch.forEach(function(fieldToSearch){
-                    toSearch[fieldToSearch.field] = $select.search;
-                });
-            }
-
+        function getSearch($select){
+            var toSearch={};
+            vm.configuracion.campos.forEach(function(campo){
+                toSearch[campo.field] = $select.search;
+                
+            });
+            vm.configuracion.subcampos.forEach(function(subcampo){
+                toSearch[subcampo.field] = $select.search;
+            });
             return toSearch;
         }
-
-        function getSearchEnabled() {
-            return vm.searchEnabled || true;
+        function getSearchEnabled(){
+        	return vm.searchEnabled || true;
         }
-
         function checkDatos() {
             if (vm.entidades === undefined || vm.entidades.length === 0) {
                 cargarDatos();
